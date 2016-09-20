301d3faf0949224adfda1c7c50bf06a576393bc5
Pavel <SereginPavel57@yandex.ru>
2016-09-21 00:49:11 +0300

    testing comit

---
+++ Sequen/src/main/java/MiniMusicPlayer3.java
@@ -0,0 +1,101 @@
+/**
+ * Created by SerP on 20.09.2016.
+ */
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.sound.midi.ControllerEventListener;
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Sequence;
+import javax.sound.midi.Sequencer;
+import javax.sound.midi.ShortMessage;
+import javax.sound.midi.Track;
+import javax.swing.*;
+
+public class MiniMusicPlayer3 {
+    static JFrame f = new JFrame("Мой первый музыкальный клип");
+    static MyDrawPanel ml;
+
+    public static void main(String[] args) {
+        MiniMusicPlayer3 mini = new MiniMusicPlayer3();
+        mini.go();
+    }
+
+    public void setUpGui(){
+        ml = new MyDrawPanel();
+        f.setContentPane(ml);
+        f.setBounds(30, 30, 300, 300);
+        f.setVisible(true);
+    }
+
+    public void go(){
+        setUpGui();
+
+        try{
+            Sequencer sequencer = MidiSystem.getSequencer();
+            sequencer.open();
+
+            int[] eventIWant = {127};
+            sequencer.addControllerEventListener(ml, eventIWant);
+
+            Sequence seq = new Sequence(Sequence.PPQ, 4);
+            Track track = seq.createTrack();
+
+            int r = 0;
+            for(int i = 5; i < 60; i+=4){
+                r = (int) (Math.random() * 50) + 1;
+                track.add(makeEvent(144, 1, r, 100, i));
+                track.add(makeEvent(176, 1, 127, 0, i));
+                track.add(makeEvent(128, 1, r, 100, i + 2));
+            }
+
+            sequencer.setSequence(seq);
+            sequencer.setTempoInBPM(120);
+            sequencer.start();
+
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public static MidiEvent makeEvent(int comd, int chan, int one, int two, int tick){
+        MidiEvent event = null;
+        try{
+            ShortMessage a = new ShortMessage();
+            a.setMessage(comd, chan, one, two);
+            event = new MidiEvent(a, tick);
+        }catch(Exception e){}
+        return event;
+    }
+
+    public class MyDrawPanel extends JPanel implements ControllerEventListener {
+        boolean msg = false;
+
+        public void controlChange(ShortMessage event){
+            msg = true;
+            repaint();
+        }
+
+        public void paintComponent(Graphics g){
+            if (msg){
+
+                Graphics2D g2 = (Graphics2D) g;
+                int red = (int) (Math.random() * 255);
+                int green = (int) (Math.random() * 255);
+                int blue = (int) (Math.random() * 255);
+
+                g.setColor(new Color(red, green, blue));
+
+                int ht = (int) ((Math.random() * 125) + 10);
+                int width = (int) ((Math.random() * 125) + 10);
+                int x = (int) ((Math.random() * 40) + 10);
+                int y = (int) ((Math.random() * 40) + 1);
+                g.fillRect(x, y, ht, width);
+                msg = false;
+            }
+
+        }
+    }
+}
